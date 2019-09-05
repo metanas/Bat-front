cf80406a082b71f90deb63bab5e73a2d7fdc720c
metanas <com.com404@gmail.com>
2019-09-05 22:37:00 +0100

    fix lint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+.home
+  img(alt="Vue logo" :src="logo")
+  HelloWorld(msg="Welcome to Your Vue.js + TypeScript App")
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import logo from "@/assets/logo.png";
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public logo = logo;
+}
 </script>
