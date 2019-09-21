f7ef73ac497e9d1b2fe6de514c3b6d0583947562
metanas <com.com404@gmail.com>
2019-09-21 19:20:21 +0100

    Svg

---
+++ src/components/Svg/index.ts
@@ -0,0 +1,7 @@
+import DataListIcon from "./DataListIcon.vue";
+import ThumbListIcon from "./ThumbListIcon.vue";
+import ImageListIcon from "./ImageListIcon.vue";
+import MenuIcon from "./MenuIcon.vue";
+import MobileMenuIcon from "./MobileMenuIcon.vue";
+
+export { DataListIcon, ThumbListIcon, ImageListIcon, MenuIcon, MobileMenuIcon };
