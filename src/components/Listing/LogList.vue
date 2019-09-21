497db31285b1a78ca627cc241b37978283918427
yassineaachir123 <yassineaachir123@gmail.com>
2019-09-21 14:40:20 +0100

    Add LogList

---
+++ src/components/Listing/LogList.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+table.table.table-sm.table-borderless
+    tbody
+        td
+            td
+                span( :class="`log-indicator align-middle ${log.color}`")
+            td
+                span.font-weight-medium {{log.label}}
+            td.text-right
+                span.text-muted {{log.time}}
+</template>
+
+<script lang="ts">
+import {Prop, Vue} from "vue-property-decorator";
+export default class LogList extends Vue{
+    @Prop() public logs: object;
+    }
+</script>
