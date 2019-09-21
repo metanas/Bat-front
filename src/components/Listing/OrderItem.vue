c9c647c1b61d2f9b60f1cc71c68adf2eb0f79f2a
yassineaachir123 <yassineaachir123@gmail.com>
2019-09-21 14:48:52 +0100

    Add OrderItem

---
+++ src/components/Listing/OrderItem.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+
+b-card.d-flex.flex-row.mb-3 (no-body)
+    .d-flex.flex-grow-1.min-width-zero
+        b-card-body.align-self-center.d-flex.flex-column.flex-md-row.justify-content-between.min-width-zero.align-items-md-center
+            router-link.list-item-heading.mb-1.truncate.w-40.w-xs-100( tag="a" :to="detailPath" ) {{data.name}}
+            p.mb-1.text-muted.text-small.w-15.w-xs-100 {{data.address}}
+            p.mb-1.text-muted.text-small.w-15.w-xs-100 {{data.date}}
+            .w-15.w-xs-100.text-right
+                b-badge(pill :variant="data.statusColor") {{data.status}}
+</template>
+
+<script lang="ts">
+import {Prop, Vue} from "vue-property-decorator";
+export default class OrderItem extends Vue {
+    @Prop() public data: object;
+    @Prop() public detailPath: string;
+
+    }
+</script>
