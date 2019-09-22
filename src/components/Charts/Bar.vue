31ba8994293478d8c985b23481d6fdd124dc9854
yassineaachir123 <yassineaachir123@gmail.com>
2019-09-22 22:58:07 +0100

    Add Bar

---
+++ src/components/Charts/Bar.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { Bar } from 'vue-chartjs'
+import { barChartOptions } from './config'
+import {Component, Prop} from "vue-property-decorator";
+@Component({
+    name:"Bar"
+})
+export default class Bar extends Bar  {
+    @Prop() public data: object;
+    public  options = barChartOptions ;
+    public mounted(): void {
+        this.renderChart(this.data, this.options)
+    }
+}
+</script>
