666b513f2ad3067be5bc7a5bbece20bd3a347cf7
yassineaachir123 <yassineaachir123@gmail.com>
2019-09-22 14:21:20 +0100

    Add TwoColumnList

---
+++ src/components/Listing/TwoColumnList.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+
+.d-flex.flex-row
+    .w-50
+        ul.list-unstyled.mb-0
+            li.mb-1( v-for="(item,index) in data.slice(0,data.length/2)" :key="index" )
+                router-link(tag="a" :to="item.link") {{item.title}}
+    .w-50
+        ul.list-unstyled.mb-0
+            li.mb-1(v-for="(item,index) in data.slice(data.length/2,data.length)" :key="index")
+                router-link(tag="a" :to="item.link") {{item.title}}
+    
+</template>
+
+<script lang="ts">
+import {Prop, Vue} from "vue-property-decorator";
+export default class TwoColumnList extends Vue {
+    @Prop() public data: object;
+    @Prop() public item: object;
+
+}
+</script>
