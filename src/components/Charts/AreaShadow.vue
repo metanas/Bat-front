d2345d698a0299f733fd35de9560994e73defccb
yassineaachir123 <yassineaachir123@gmail.com>
2019-09-23 12:03:13 +0100

    Add AreaShadow

---
+++ src/components/Charts/AreaShadow.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import Chart from 'chart.js'
+import { generateChart } from 'vue-chartjs'
+import { areaChartOptions } from './config'
+import {Component, Prop, Vue} from "vue-property-decorator";
+
+Chart.defaults.lineWithShadow = Chart.defaults.line
+Chart.controllers.lineWithShadow = Chart.controllers.line.extend({
+    draw: function (ease) {
+        Chart.controllers.line.prototype.draw.call(this, ease)
+        var ctx = this.chart.ctx;
+        ctx.save();
+        ctx.shadowColor = 'rgba(0,0,0,0.15)';
+        ctx.shadowBlur = 10;
+        ctx.shadowOffsetX = 0;
+        ctx.shadowOffsetY = 10;
+        ctx.responsive = true;
+        ctx.stroke();
+        Chart.controllers.line.prototype.draw.apply(this, arguments)
+        ctx.restore()
+    }
+});
+const AreaShadow = generateChart('area-with-shadow', 'lineWithShadow')
+
+@Component({
+    name:"AreaShadow"
+})
+
+export default class AreaShadow extends AreaShadow {
+    @Prop() public data: object;
+
+    public  options = areaChartOptions;
+    public mounted(): void {
+        this.renderChart(this.data, this.options)
+    }
+
+}
+</script>
