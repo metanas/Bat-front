1d246c72b00911cea11b0915c20f3d9d85ab87d2
yassineaachir123 <yassineaachir123@gmail.com>
2019-09-23 12:51:49 +0100

    Add Scatter

--- src/components/Charts/Area.vue
+++ src/components/Charts/Area.vue
@@ -9,7 +9,6 @@ import { areaChartOptions } from './config'
 
 export default class Area extends Line {
     @Prop() public data: object;
-
     public  options = areaChartOptions;
     public mounted(): void {
         this.renderChart(this.data, this.options)
---
+++ src/components/Charts/Scatter.vue
@@ -0,0 +1,21 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { Scatter } from 'vue-chartjs'
+import { scatterChartOptions } from './config'
+import {Component, Prop, Vue} from "vue-property-decorator";
+@Component({
+    name:"ScatterClass"
+})
+export default class ScatterClass extends Scatter {
+    @Prop() public data: object;
+
+    public  options = scatterChartOptions;
+    public mounted(): void {
+        this.renderChart(this.data, this.options)
+    }
+}
+</script>
+
