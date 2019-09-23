87802b45a8fb3ab14dd44e7909fa0f0454339129
yassineaachir123 <yassineaachir123@gmail.com>
2019-09-23 12:40:26 +0100

    Add PolarArea

---
+++ src/components/Charts/PolarArea.vue
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+import { PolarArea } from 'vue-chartjs'
+import {Component, Prop, Vue} from "vue-property-decorator";
+import { polarAreaChartOptions } from './config'
+@Component({
+    name:"PolarAreaClass"
+})
+export default class PolarAreaClass extends PolarArea {
+    @Prop() public data: object;
+
+    public  options = polarAreaChartOptions;
+    public mounted(): void {
+        this.renderChart(this.data, this.options)
+    }
+}
+</script>
