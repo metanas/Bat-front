3876b09ba2c9e6475a1e7ed5efe77f3e7bad628b
yassineaachir123 <yassineaachir123@gmail.com>
2019-09-23 12:45:59 +0100

    Add Radar

---
+++ src/components/Charts/Radar.vue
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+import { Radar } from 'vue-chartjs'
+import { radarChartOptions } from './config'
+import {Component, Prop, Vue} from "vue-property-decorator";
+@Component({
+    name:"RadarClass"
+})
+export default class RadarClass extends Radar {
+    @Prop() public data: object;
+
+    public  options = radarChartOptions;
+    public mounted(): void {
+        this.renderChart(this.data, this.options)
+    }
+}
+</script>
