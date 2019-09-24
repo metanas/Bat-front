ca63fd524fd023a813e47cf0eb4a803bdb331248
yassineaachir123 <yassineaachir123@gmail.com>
2019-09-24 14:09:06 +0100

    Add ApplicationMenu

---
+++ src/components/Common/ApplicationMenu.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+    div(:class="{'app-menu':true, 'shown' : isOpen}"  @mouseenter="isMenuOver=true" @mouseleave="isMenuOver=false")
+        slot
+        a.app-menu-button.d-inline-block.d-xl-none(@click.prevent="toggle")
+            i.simple-icon-options
+</template>
+
+<script lang="ts">
+    import {Component,  Vue} from "vue-property-decorator";
+    import { setTimeout } from 'timers'
+
+    @Component({
+        name: "ApplicationMenu"})
+
+    export default class ApplicationMenu extends Vue{
+
+        public isOpen: false,
+        public isMenuOver: false
+
+        public addEvents(){
+            document.addEventListener('click', this.handleDocumentClick)
+            document.addEventListener('touchstart', this.handleDocumentClick)
+        }
+
+        public removeEvents(){
+            document.removeEventListener('click', this.handleDocumentClick)
+            document.removeEventListener('touchstart', this.handleDocumentClick)
+        }
+
+        public handleDocumentClick(e?:any){
+            if (!this.isMenuOver) {
+                this.toggle()
+            }
+        }
+        public toggle (){
+            this.isOpen = !this.isOpen
+
+        }
+
+    }
+</script>
+
